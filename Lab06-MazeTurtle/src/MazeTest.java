8e64db168d4190c3b9b9cc478f23445a1852f8aa
kar-re <kaspian.jakobsson@gmail.com>
2020-01-22 08:12:10 +0100

    Lab 06 complete :fire: :turtle:

---
+++ Lab06-MazeTurtle/src/MazeTest.java
@@ -0,0 +1,39 @@
+import se.lth.cs.pt.maze.Maze;
+import se.lth.cs.pt.window.SimpleWindow;
+
+/*
+ * Tester for MazeWalker
+ * */
+public class MazeTest {
+
+	public static void main(String[] args) {
+
+		/* Initial variables and things that need to be instanced */
+		SimpleWindow window;
+		Turtle turtle;
+		Maze maze;
+		int mazeNbr = 0;
+
+		/* Initial screen, choose maze */
+		window = new SimpleWindow(512, 512, "MazeTest");
+		window.moveTo(window.getWidth() / 2, window.getHeight() / 2);
+		window.writeText("Vilken labyrint?");
+		mazeNbr = Character.getNumericValue(window.waitForKey());
+		window.clear();
+
+		/* Instantiate maze and turtle, as well as mazewalker */
+		maze = new Maze(mazeNbr);
+		turtle = new Turtle(window, maze.getXEntry(), maze.getYEntry());
+		MazeWalker mw = new MazeWalker(turtle);
+		turtle.penDown();
+		maze.draw(window);
+		mw.walk(maze);
+		/* Loop for mazewalker */
+		while (!maze.atExit(turtle.getX(), turtle.getY())) {
+			mw.walk(maze);
+			window.delay(10);
+		}
+
+	}
+
+}
---
+++ Lab06-MazeTurtle/src/MazeWalker.java
@@ -0,0 +1,35 @@
+import se.lth.cs.pt.maze.Maze;
+/*
+ * Super simple algorithm to traverse maze using the 
+ * left hand method supplied according to the lab instructions
+ */
+
+public class MazeWalker {
+	private Turtle turtle;
+	private int d, x, y;
+
+	public MazeWalker(Turtle t) {
+		turtle = t;
+	}
+
+	public void walk(Maze maze) {
+		/* Get direction and positions, put in variables */
+		d = turtle.getDirection();
+		x = turtle.getX();
+		y = turtle.getY();
+
+		/* Left and front collision check, and move */
+		if (maze.wallAtLeft(d, x, y)) {
+			if (maze.wallInFront(d, x, y)) {
+				turtle.left(-90);
+				turtle.forward(1);
+			} else {
+				turtle.forward(1);
+			}
+
+		} else {
+			turtle.left(90);
+			turtle.forward(1);
+		}
+	}
+}
